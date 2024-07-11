02abcc0ff862314febd6c6dcff5ce4984d9c7847
Sameer Kumar <sameerkumar1012.sk@gmail.com>
2024-07-11 14:56:36 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
